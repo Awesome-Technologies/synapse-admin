e62ac37b7ae87197182a9008df2a9484e4a7edf2
Lukas Winkler <git@lw1.at>
2020-02-10 19:20:20 +0100

    fix HTTP DELETE

--- src/synapse/dataProvider.js
+++ src/synapse/dataProvider.js
@@ -207,13 +207,14 @@ const dataProvider = {
     const res = resourceMap[resource];
     const homeserver_url = "https://" + homeserver + res.path;
     return Promise.all(
-      params.ids.map(id => jsonClient(`${homeserver_url}/${id}`), {
-        method: "DELETE",
-        body: JSON.stringify(params.data, filterNullValues),
-      })
-    ).then(responses => ({
-      data: responses.map(({ json }) => json),
-    }));
+      params.ids.map(id => jsonClient(`${homeserver_url}/${id}`, {
+          method: "DELETE",
+          body: JSON.stringify(params.data, filterNullValues),
+        },
+      ).then(responses => ({
+        data: responses.map(({ json }) => json),
+      })))
+    );
   },
 };
 
