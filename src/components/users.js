68e5a72618c70632d490da9a3f3ac34cabb69fdb
dklimpel <5740567+dklimpel@users.noreply.github.com>
2020-03-28 22:31:59 +0100

    Increase export of users up to 5000

--- src/components/users.js
+++ src/components/users.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { cloneElement } from "react";
 import {
   Datagrid,
   Create,
@@ -14,7 +14,49 @@ import {
   TextInput,
   ReferenceField,
   regex,
+  CreateButton,
+  ExportButton,
+  TopToolbar,
+  sanitizeListRestProps,
 } from "react-admin";
+
+const ListActions = ({
+    currentSort,
+    className,
+    resource,
+    filters,
+    displayedFilters,
+    exporter, // you can hide ExportButton if exporter = (null || false)
+    filterValues,
+    permanentFilter,
+    hasCreate, // you can hide CreateButton if hasCreate = false
+    basePath,
+    selectedIds,
+    onUnselectItems,
+    showFilter,
+    maxResults,
+    total,
+    ...rest
+}) => (
+    <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+        {filters && cloneElement(filters, {
+            resource,
+            showFilter,
+            displayedFilters,
+            filterValues,
+            context: 'button',
+        })}
+      <CreateButton basePath={basePath} />
+      <ExportButton
+        disabled={total === 0}
+        resource={resource}
+        sort={currentSort}
+        filter={{ ...filterValues, ...permanentFilter }}
+        exporter={exporter}
+        maxResults={maxResults}
+      />
+    </TopToolbar>
+);
 
 const UserFilter = props => (
   <Filter {...props}>
@@ -33,6 +75,7 @@ export const UserList = props => (
     filters={<UserFilter />}
     filterDefaultValues={{ guests: true, deactivated: false }}
     bulkActionButtons={false}
+    actions={<ListActions maxResults={10000} />}
   >
     <Datagrid rowClick="edit">
       <ReferenceField
