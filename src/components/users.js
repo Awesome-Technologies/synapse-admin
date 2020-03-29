d805dcd2c699bdb63fed60e257204d92d6389d19
dklimpel <5740567+dklimpel@users.noreply.github.com>
2020-03-29 20:59:54 +0200

    Fix coding style

--- src/components/users.js
+++ src/components/users.js
@@ -21,41 +21,41 @@ import {
 } from "react-admin";
 
 const ListActions = ({
-    currentSort,
-    className,
-    resource,
-    filters,
-    displayedFilters,
-    exporter, // you can hide ExportButton if exporter = (null || false)
-    filterValues,
-    permanentFilter,
-    hasCreate, // you can hide CreateButton if hasCreate = false
-    basePath,
-    selectedIds,
-    onUnselectItems,
-    showFilter,
-    maxResults,
-    total,
-    ...rest
+  currentSort,
+  className,
+  resource,
+  filters,
+  displayedFilters,
+  exporter, // you can hide ExportButton if exporter = (null || false)
+  filterValues,
+  permanentFilter,
+  hasCreate, // you can hide CreateButton if hasCreate = false
+  basePath,
+  selectedIds,
+  onUnselectItems,
+  showFilter,
+  maxResults,
+  total,
+  ...rest
 }) => (
-    <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
-        {filters && cloneElement(filters, {
-            resource,
-            showFilter,
-            displayedFilters,
-            filterValues,
-            context: 'button',
-        })}
-      <CreateButton basePath={basePath} />
-      <ExportButton
-        disabled={total === 0}
-        resource={resource}
-        sort={currentSort}
-        filter={{ ...filterValues, ...permanentFilter }}
-        exporter={exporter}
-        maxResults={maxResults}
-      />
-    </TopToolbar>
+  <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+      {filters && cloneElement(filters, {
+          resource,
+          showFilter,
+          displayedFilters,
+          filterValues,
+          context: 'button',
+      })}
+    <CreateButton basePath={basePath} />
+    <ExportButton
+      disabled={total === 0}
+      resource={resource}
+      sort={currentSort}
+      filter={{ ...filterValues, ...permanentFilter }}
+      exporter={exporter}
+      maxResults={maxResults}
+    />
+  </TopToolbar>
 );
 
 const UserFilter = props => (
