12447b77089608b0a41dec2eb678379cd9c84b7a
Manuel Stahl <manuel.stahl@awesome-technologies.de>
2020-07-13 10:14:49 +0200

    Increase export of users up to 10000
    Change-Id: I54c7a52ae35aeb311074f0f3b103a2fdb92aaedd

--- src/components/users.js
+++ src/components/users.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { cloneElement, Fragment } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
 import ContactMailIcon from "@material-ui/icons/ContactMail";
@@ -30,6 +30,10 @@ import {
   regex,
   useTranslate,
   Pagination,
+  CreateButton,
+  ExportButton,
+  TopToolbar,
+  sanitizeListRestProps,
 } from "react-admin";
 import { ServerNoticeButton, ServerNoticeBulkButton } from "./ServerNotices";
 import { makeStyles } from "@material-ui/core/styles";
@@ -45,6 +49,45 @@ const useStyles = makeStyles({
     float: "right",
   },
 });
+
+const UserListActions = ({
+  currentSort,
+  className,
+  resource,
+  filters,
+  displayedFilters,
+  exporter, // you can hide ExportButton if exporter = (null || false)
+  filterValues,
+  permanentFilter,
+  hasCreate, // you can hide CreateButton if hasCreate = false
+  basePath,
+  selectedIds,
+  onUnselectItems,
+  showFilter,
+  maxResults,
+  total,
+  ...rest
+}) => (
+  <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+    {filters &&
+      cloneElement(filters, {
+        resource,
+        showFilter,
+        displayedFilters,
+        filterValues,
+        context: "button",
+      })}
+    <CreateButton basePath={basePath} />
+    <ExportButton
+      disabled={total === 0}
+      resource={resource}
+      sort={currentSort}
+      filter={{ ...filterValues, ...permanentFilter }}
+      exporter={exporter}
+      maxResults={maxResults}
+    />
+  </TopToolbar>
+);
 
 const UserPagination = props => (
   <Pagination {...props} rowsPerPageOptions={[10, 25, 50, 100, 500, 1000]} />
@@ -86,6 +129,7 @@ export const UserList = props => {
       {...props}
       filters={<UserFilter />}
       filterDefaultValues={{ guests: true, deactivated: false }}
+      actions={<UserListActions maxResults={10000} />}
       bulkActionButtons={<UserBulkActionButtons />}
       pagination={<UserPagination />}
     >
