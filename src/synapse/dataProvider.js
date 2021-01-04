88e93d0fec669a576712424fa9bccd0ebf5b1db4
dklimpel <5740567+dklimpel@users.noreply.github.com>
2021-01-04 10:14:46 +0100

    Update pushers `total` and `id`

--- src/synapse/dataProvider.js
+++ src/synapse/dataProvider.js
@@ -105,11 +105,15 @@ const resourceMap = {
   pushers: {
     map: p => ({
       ...p,
+      id: p.pushkey,
     }),
     reference: id => ({
       endpoint: `/_synapse/admin/v1/users/${id}/pushers`,
     }),
     data: "pushers",
+    total: json => {
+      return json.total;
+    },
   },
   servernotices: {
     map: n => ({ id: n.event_id }),
