c8082a719817abae5198ce9063996270779c8023
Manuel Stahl <manuel.stahl@awesome-technologies.de>
2021-05-04 19:16:21 +0200

    Remove TestContext from App.test.js
    The TestContext is only required for components that depend on react-admin,
    but not for the Admin component itself.
    
    Change-Id: I3e07cb6bfa592f1bf59ca282cdf1c2e6c922f619

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { TestContext } from "ra-test";
 import { shallow } from "enzyme";
 import App from "./App";
 
 describe("App", () => {
   it("renders", () => {
-    shallow(
-      <TestContext>
-        <App />
-      </TestContext>
-    );
+    shallow(<App />);
   });
 });
