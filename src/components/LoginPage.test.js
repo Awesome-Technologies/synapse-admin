fccc52820e3bf507f8c022519d2c59a0b7adcb6e
dklimpel <5740567+dklimpel@users.noreply.github.com>
2023-02-03 15:41:14 +0100

    fix imports

--- src/components/LoginPage.test.js
+++ src/components/LoginPage.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { TestContext } from "ra-test";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import LoginPage from "./LoginPage";
 
 describe("LoginForm", () => {
