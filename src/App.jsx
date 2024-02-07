bc4b2d0f955c121bb00bb21e0c021f24900a2266
dklimpel <5740567+dklimpel@users.noreply.github.com>
2024-02-07 20:05:48 +0100

    set `palette` to `darkTheme`

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const App = () => (
     authProvider={authProvider}
     dataProvider={dataProvider}
     i18nProvider={i18nProvider}
-    darkTheme
+    darkTheme={{ palette: { mode: "dark" } }}
   >
     <CustomRoutes>
       <Route path="/import_users" element={<ImportFeature />} />
