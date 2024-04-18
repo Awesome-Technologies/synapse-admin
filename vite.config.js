a8792804d1752622505d7c2deadb7cfd61033abe
Manuel Stahl <manuel.stahl@awesome-technologies.de>
2024-04-18 20:25:14 +0200

    Use vite-plugin-version-mark to read project version
    Change-Id: I0d46dc57df025538379a2f0786d3e972c56dd248

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { vitePluginVersionMark } from "vite-plugin-version-mark";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginVersionMark({
+      command: "git describe --tags",
+      ifMeta: true,
+      ifLog: true,
+      ifGlobal: true,
+    }),
+  ],
 });
