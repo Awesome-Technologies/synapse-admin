725f839870f8a8cd9a6c581627eacfcbc9bc8fa7
Navy <navydotgif@gmail.com>
2026-02-25 14:59:26 +0200

    fix: Media is beind auth, handle case where avatar is a Promise<Blob>

--- src/components/AvatarField.tsx
+++ src/components/AvatarField.tsx
@@ -2,11 +2,28 @@ import { get } from "lodash";
 
 import { Avatar } from "@mui/material";
 import { useRecordContext } from "react-admin";
+import { useEffect, useRef, useState } from "react";
 
 const AvatarField = ({ source, ...rest }) => {
   const record = useRecordContext(rest);
-  const src = get(record, source)?.toString();
+  const _src = get(record, source);
   const { alt, classes, sizes, sx, variant } = rest;
+
+  const [src, setSrc] = useState('');
+  useEffect(() => {
+    (async () => {
+      const raw = await _src;
+      if(!raw)
+        return;
+
+      if(typeof raw === 'string'){
+        setSrc(raw);
+      } else { // Blob
+        const url = window.URL.createObjectURL(raw);
+        setSrc(url);
+      }
+    })();
+  }, [_src]);
   return <Avatar alt={alt} classes={classes} sizes={sizes} src={src} sx={sx} variant={variant} />;
 };
 
